9af9f1f5bc2f139667a398133fefbba8aaa7556d
vrm <sotormd@proton.me>
2026-04-08 01:24:47 +0530

    fix: scripts

--- module/scripts.nix
+++ module/scripts.nix
@@ -1,13 +1,8 @@
 { pkgs, ... }:
 
 let
-  switchScript = pkgs.writeShellScriptBin "switch" "";
-
-  cleanScript = pkgs.writeShellScriptBin "clean" ''
-    #!${pkgs.runtimeShell}
-
-        nix-collect-garbage --delete-old
-  '';
+  switchScript = pkgs.writeShellScriptBin "switch" "nix-on-droid switch --flake github:sotormd/droid";
+  cleanScript = pkgs.writeShellScriptBin "clean" "nix-collect-garbage --delete-old";
 in
 {
   environment.packages = [
